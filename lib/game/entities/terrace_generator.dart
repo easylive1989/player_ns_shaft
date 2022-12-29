e374a042b1a6dd74f98e6e581aa38fabeabddf59
Paul <easylive1989@gmail.com>
2022-12-29 09:24:46 +0800

    update terrance generate logic

--- lib/game/entities/terrace_generator.dart
+++ lib/game/entities/terrace_generator.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:player_ns_shaft/game/entities/terrace.dart';
 import 'package:player_ns_shaft/game/game.dart';
@@ -9,8 +11,17 @@ class TerraceGenerator extends Component with HasGameRef<VeryGoodFlameGame> {
 
   @override
   void update(double dt) {
+    final random = Random();
+    double currentGenerateSize = -50;
     if (player.position.y % 70 == 30 && player.position.y > 100) {
-      gameRef.add(Terrace(position: Vector2.copy(player.position)..add(Vector2(0, 50))));
+      while (currentGenerateSize < gameRef.size.x + 100) {
+        final nextInt = random.nextInt(150);
+        currentGenerateSize += nextInt;
+        gameRef.add(
+          Terrace(position: Vector2(currentGenerateSize, player.position.y + 200)),
+        );
+        currentGenerateSize += 50;
+      }
     }
   }
 }
